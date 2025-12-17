dfe3b67a4fa6277568b02d20bcf28ad34428d744
Juan Sapriza <juan.sapriza@epfl.ch>
2025-12-17 13:38:31 +0000

    Added comments to warn about limits, and removed plotting by default, which introduces aliasing

--- sw/applications/test_dsm_selfdrive/evaluation.py
+++ sw/applications/test_dsm_selfdrive/evaluation.py
@@ -4,35 +4,46 @@
 %matplotlib widget
 import numpy as np
 import matplotlib.pyplot as plt
-
+import re
 
 header = open("output.txt").readline().strip()
 x, y = np.loadtxt("output.txt", delimiter="\t", skiprows=1, unpack=True)
 
 plt.figure()
 plt.title(header)
-# plt.plot(x, y, marker="o")
-plt.scatter(x, y)
+plt.plot(x, y, marker="o")
+# plt.scatter(x, y)
 plt.xlabel("x")
 plt.ylabel("y")
 plt.show()
 
-#In[]:
+#In[]
 
-data = np.array([v for v in np.array(y)])  # dataour data-column
-MOD = 1024
+length_n = len(y)
+p2          = np.floor(np.log2(length_n))
 
-data_fixed = data.copy()
-for i in range(1, len(data_fixed)):
-    if data_fixed[i] - data_fixed[i-1] < -MOD//2:
-        data_fixed[i:] += MOD
-
-# optional: center around 0
-data_fixed -= MOD//2
+y_chopped   = y[- int(2**p2):]
+x_chopped   = x[- int(2**p2):]
 
 plt.figure()
-plt.title(header)
-plt.plot(x, data_fixed, marker="o")
-plt.xlabel("x")
-plt.ylabel("y")
+plt.plot(x_chopped,y_chopped)
+plt.show()
+
+m               = re.search(r"fclk:(\d+)\s*Hz.*DF:(\d+)", header)
+f_clk_Hz        = int(m.group(1))
+df              = int(m.group(2))
+
+real_f_clk_Hz   = 1e6
+fs_Hz           = real_f_clk_Hz/df
+
+N = len(y_chopped)
+Y = np.fft.fft(y_chopped)
+f = np.fft.fftfreq(N, d=1/fs_Hz)
+
+plt.figure()
+plt.plot(f[:N//2], np.abs(Y[:N//2]))
+plt.xlabel("Hz")
+plt.ylabel("|FFT|")
+plt.xscale('log')
+plt.yscale('log')
 plt.show()
